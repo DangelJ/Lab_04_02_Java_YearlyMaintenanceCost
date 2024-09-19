7b1fecf3357e35881841252bc1472a88635784be
Jonah <dangelja@mail.uc.edu>
2024-09-19 13:37:52 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double springCost = 2534.87;
+        double summerCost = 2746.34;
+        double fallCost = 2345.49;
+        double winterCost = 2854.16;
+        double totalCost = 0;
+
+        totalCost = winterCost + summerCost + fallCost + springCost;
+
+        System.out.println("Your total cost for the year is $" + totalCost);
+
+
+    }
+}
